7e6fbbca107d8ef623842e36318f48b4b87ce618
Prashant Sahni <prashant.sahni5@gmail.com>
2014-01-21 18:32:04 +0530

    show action added

--- s_app/app.rb
+++ s_app/app.rb
@@ -11,10 +11,13 @@ require './config'
 
 set :database, 'sinatra_app_dev'
 
+
 configure do
   set :views, File.join(File.dirname(__FILE__), 'views')
   DatabaseTasks.establish_connection()
 end
+
+
 
 
 # After creating the database and defining the migrations
@@ -24,10 +27,23 @@ end
 
 class MyApp < Sinatra::Base
 
+  helpers do
+
+    def post_link(post, title)
+      "<a href='/posts/#{ post.id }'>#{ title }</a>"
+    end
+
+  end
+
  get '/' do
     @posts = Post.order('created_at DESC')
     haml :index
  end
 
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    haml :show
+  end
 end
 
